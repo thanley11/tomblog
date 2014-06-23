267a005038ba9c9abcfcdf3613c21a0482ee5433
Tom Hanley <thanley11@gmail.com>
2014-06-23 14:28:17 +0000

    Template folder and updated urls.py

---
+++ blogengine/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+from django.views.generic import ListView
+from blogengine.models import Post
+
+urlpatterns = patterns('',
+        url('^$', ListView.as_view(
+            model=Post,
+        )),
+)
