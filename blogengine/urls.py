0c5d96c9697b04f341f5f5286928c06d396b48b7
Tom Hanley <thanley11@gmail.com>
2014-06-30 17:56:41 +0000

    Added CategoryListView

--- blogengine/urls.py
+++ blogengine/urls.py
@@ -1,13 +1,21 @@
 from django.conf.urls import patterns, url
 from django.views.generic import ListView, DetailView
-from blogengine.models import Post
+from blogengine.models import Post, Category
+from blogengine.views import CategoryListView
 
 urlpatterns = patterns('',
+        # Index
         url(r'^(?P<page>\d+)?/?$', ListView.as_view(
             model=Post,
             paginate_by=5,
         )),
-       url(r'^(?P<pub_date__year>\d{4})/(?P<pub_date__month>\d{1,2})/(?P<pub_date__day>\d{1,2})/(?P<slug>[a-zA-Z0-9-]+)/?$',  DetailView.as_view(
+        # Individual posts
+        url(r'^(?P<pub_date__year>\d{4})/(?P<pub_date__month>\d{1,2})/(?P<pub_date__day>\d{1,2})/(?P<slug>[a-zA-Z0-9-]+)/?$',  DetailView.as_view(
             model=Post,
+        )),
+        # Categories
+        url(r'^category/(?P<slug>[a-zA-Z0-9-]+)/?$', CategoryListView.as_view(
+            paginate_by=5,
+            model=Category,
         )),
 )
